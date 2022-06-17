22d63a239839c90feb3cbf98c4d7362640f3e416
irfan701 <irfanhossain701@gmail.com>
2022-06-17 20:31:14 +0400

    Undefined Route
    404 Handler

--- API/app.js
+++ API/app.js
@@ -5,4 +5,9 @@ const app=new express();
 
 app.use("/v1/api/",router)
 
+//undefined route
+app.use("*",(req,res)=>{
+    res.status(404).json({status:"Fail",data:"Not Found"})
+});
+
 module.exports=app;
