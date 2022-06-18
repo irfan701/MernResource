43f491b6c95e91af390ff6b59e7177377058f14a
irfan701 <irfanhossain701@gmail.com>
2022-06-18 18:05:03 +0400

    ENV SETUP
    ENV SETUP

--- API/index.js
+++ API/index.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const dotenv=require('dotenv');
+
+dotenv.config({path:'./config.env'})
 
 
-app.listen(4000,()=>{
+app.listen(process.env.RUNNING_PORT,()=>{
     console.log("SERVER RUN SUCCESS");
 })
