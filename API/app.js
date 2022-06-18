4f10195a1a025dd8fd4b47c4abafbc2c04d24280
irfan701 <irfanhossain701@gmail.com>
2022-06-18 17:42:55 +0400

    Security Middleware
    Security Middleware

--- API/app.js
+++ API/app.js
@@ -3,6 +3,25 @@ const router = require("./src/routes/api");
 
 const app=new express();
 
+//Security Middleware Import
+
+const rateLimit=require("express-rate-limit")
+const helmet=require("helmet")
+const mongoSanitize=require("express-mongo-sanitize")
+const xss=require("xss-clean")
+const hpp=require("hpp")
+const cors=require("cors")
+
+
+//Security Middleware Implement
+
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+
 app.use("/v1/api/",router)
 
 //undefined route
