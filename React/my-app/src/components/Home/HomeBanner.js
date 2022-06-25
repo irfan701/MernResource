8cdcab572560011e3abda6a6b358511fb3377f64
irfan701 <irfanhossain701@gmail.com>
2022-06-25 17:16:10 +0400

    useState
    useState

--- React/my-app/src/components/Home/HomeBanner.js
+++ React/my-app/src/components/Home/HomeBanner.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const HomeBanner = (props) => {
+
+    const[color,setColor]=useState('RED');
+
+    const NewColor=()=>{
+        setColor("BLUE")
+    }
+
     return (
         <div>
+            <p>PROPS LESSON</p>
             <h2>Home Banner</h2>
             <h3>{props.title}</h3>
             <h4>{props.subtitle}</h4>
@@ -11,6 +19,13 @@ const HomeBanner = (props) => {
 
             <p>{props.jsonObj.name}</p>
             <p>{props.jsonObj.age}</p>
+
+
+            <hr/>
+
+        <p>STATE LESSON</p>
+            <p>{color}</p>
+            <button onClick={NewColor}>Change Color</button>
         </div>
     );
 };
