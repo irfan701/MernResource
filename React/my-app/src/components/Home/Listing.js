8014b6fcc900867d799691de8a1683dd32e343a7
irfan701 <irfanhossain701@gmail.com>
2022-06-26 00:55:29 +0400

    CRUD
    CRUD

--- React/my-app/src/components/Home/Listing.js
+++ React/my-app/src/components/Home/Listing.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const Listing = () => {
-    const[todo, SetTodo] = useState([])
+    const [todo, SetTodo] = useState([])
 
     useEffect(() => {
 
@@ -11,27 +11,56 @@ const Listing = () => {
         }).catch((error) => {
 
         })
-    },[])
+    }, [])
 
-    const myList = todo.map((child,index)=>{
+    const DeleteEvent = (id) => {
+        alert(id)
+    }
+    const EditEvent = (id) => {
+        alert(id)
+    }
+    const DetailsEvent = (id) => {
+        alert(id)
+    }
+
+    const myList = todo.map((child, index) => {
         return (
             <tr>
                 <td>{child['userId']}</td>
                 <td>{child['id']}</td>
                 <td>{child['title']}</td>
-                <td>{child['completed']}</td>
+                <td>
+                    <button onClick={EditEvent.bind(this, child.id)} className="btn btn-secondary">Edit</button>
+                    <button onClick={DeleteEvent.bind(this, child.id)} className="btn btn-danger">Delete</button>
+                    <button onClick={DetailsEvent.bind(this, child.id)} className="btn btn-primary">Details</button>
+                </td>
 
             </tr>
-            );
+        );
 
     });
 
     return (
         <div>
             <h2>Listing</h2>
-            <table>
-                {myList}
-            </table>
+            <div className="container">
+                <div className="row">
+                    <table className="table table-bordered">
+
+                        <tr>
+                            <th>User Id</th>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Action</th>
+                        </tr>
+                        <tbody>
+                        {myList}
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+
 
         </div>
     );
