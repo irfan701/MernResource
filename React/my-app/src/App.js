59093945402dd61bc0fec06e0af1af4f8f35c81b
irfan701 <irfanhossain701@gmail.com>
2022-06-25 16:52:46 +0400

    React Js Project Structure
    React Js Project Structure

---
+++ React/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeBanner from "./components/Home/HomeBanner";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+    return (
+        <div>
+
+         <HomePage/>
+        </div>
+)
+    ;
+};
+
+export default App;
---
+++ React/my-app/src/components/Home/HomeBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeBanner = () => {
+    return (
+        <div>
+            <h2>Home Banner</h2>
+        </div>
+    );
+};
+
+export default HomeBanner;
