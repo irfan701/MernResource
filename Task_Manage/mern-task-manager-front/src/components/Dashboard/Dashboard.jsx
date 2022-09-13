b3decf4ba21129fa951759517b3bdedc65d3c374
irfan701 <irfanhossain701@gmail.com>
2022-09-14 01:57:53 +0400

    Create View
    Create View or Template

--- Task_Manage/mern-task-manager-front/src/components/Dashboard/Dashboard.jsx
+++ Task_Manage/mern-task-manager-front/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import MasterLayout from "../masterLayout/MasterLayout";
+import React, {Fragment} from 'react';
+
 
 const Dashboard = () => {
     return (
-        <>
-            <h2>Dashboard</h2>
-        </>
+        <Fragment>
+            <div className="container">
+                <div className="row">
+
+                            <div  className="col-12 col-lg-3 col-sm-6 col-md-3  p-2">
+                                <div className="card h-100">
+                                    <div className="card-body">
+                                        <h5 className="animated fadeInUp">Total item._id</h5>
+                                        <h6 className="text-secondary animated fadeInUp">item.sum</h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                </div>
+            </div>
+        </Fragment>
     );
 };
 
